da728af2d6af010fde4035746df2ec2c6e61b364
Zsolt Kecskés <kzsolt1984@gmail.com>
2015-10-04 21:21:31 +0200

    no message

--- pocketguide/Bundle_DMC/js/main.js
+++ pocketguide/Bundle_DMC/js/main.js
@@ -144,14 +144,8 @@ var Main;
             if (window.location.hash.indexOf('stories') !== -1) {
                 this._setAudioPlayer();
             }
-            /*$(window).on('hashchange', function() {
-                var hash = window.location.hash.replace(/^#/,'');
-                alert(hash);
-                return false;
-            });*/
         }
         Main.prototype._setAudioPlayer = function () {
-            console.log('LEFUTOOOK');
             $.each($('.box'), function (key, value) {
                 new component.AudioPlyrComponent({
                     box: value
@@ -168,30 +162,4 @@ var Main;
     doc.addEventListener('DOMContentLoaded', function () {
         new Main.Main();
     });
-    /*window.onload = function () {
-        if (typeof history.pushState === "function") {
-            history.pushState("jibberish", null, null);
-            window.onpopstate = function () {
-                history.pushState('newjibberish', null, null);
-                // Handle the back (or forward) buttons here
-                // Will NOT handle refresh, use onbeforeunload for this.
-            };
-        }
-        else {
-            var ignoreHashChange = true;
-            window.onhashchange = function () {
-                if (!ignoreHashChange) {
-                    ignoreHashChange = true;
-                    window.location.hash = Math.random();
-                    // Detect and redirect change here
-                    // Works in older FF and IE9
-                    // * it does mess with your hash symbol (anchor?) pound sign
-                    // delimiter on the end of the URL
-                }
-                else {
-                    ignoreHashChange = false;
-                }
-            };
-        }
-    }*/
 }(document, window, navigator));
--- pocketguide/Bundle_DMC/ts/main.ts
+++ pocketguide/Bundle_DMC/ts/main.ts
@@ -12,8 +12,6 @@ module Main {
         private setAudioPlayer: boolean = false;
 
         constructor() {
-
-
             new component.TeaserVideoPlayer({
                'videoId' : '6815537',
                 'youtubeId' : 'FKHWcd2wA30'
@@ -39,16 +37,9 @@ module Main {
             if(window.location.hash.indexOf('stories') !== -1) {
                 this._setAudioPlayer();
             }
-
-
-            /*$(window).on('hashchange', function() {
-                var hash = window.location.hash.replace(/^#/,'');
-                alert(hash);
-                return false;
-            });*/
         }
 
-        private _setAudioPlayer() {console.log('LEFUTOOOK')
+        private _setAudioPlayer() {
             $.each($('.box'), function(key, value) {
                 new component.AudioPlyrComponent(
                     {
@@ -59,18 +50,6 @@ module Main {
 
             this.setAudioPlayer = true;
         }
-
-        /*private changeUrl(page, url) {alert('32323')
-            if (typeof (history.pushState) != "undefined") {
-                var obj = { Page: page, Url: url };
-                history.pushState(obj, obj.Page, obj.Url);
-            } else {
-                alert("Browser does not support HTML5.");
-            }
-
-            return false;
-        }*/
-
     }
 }
 
@@ -82,31 +61,5 @@ module Main {
         new Main.Main();
     });
 
-    /*window.onload = function () {
-        if (typeof history.pushState === "function") {
-            history.pushState("jibberish", null, null);
-            window.onpopstate = function () {
-                history.pushState('newjibberish', null, null);
-                // Handle the back (or forward) buttons here
-                // Will NOT handle refresh, use onbeforeunload for this.
-            };
-        }
-        else {
-            var ignoreHashChange = true;
-            window.onhashchange = function () {
-                if (!ignoreHashChange) {
-                    ignoreHashChange = true;
-                    window.location.hash = Math.random();
-                    // Detect and redirect change here
-                    // Works in older FF and IE9
-                    // * it does mess with your hash symbol (anchor?) pound sign
-                    // delimiter on the end of the URL
-                }
-                else {
-                    ignoreHashChange = false;
-                }
-            };
-        }
-    }*/
 }(document, window, navigator));
 
