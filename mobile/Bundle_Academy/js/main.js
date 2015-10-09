612aeef0097a14d9a101818344ea5346ab99b5d6
Zsolt Kecskés <kzsolt1984@gmail.com>
2015-10-09 13:43:51 +0200

    no message

--- mobile/Bundle_Academy/js/main.js
+++ mobile/Bundle_Academy/js/main.js
@@ -11,8 +11,7 @@ var component;
             this._createVideo();
         }
         TeaserVideoPlayer.prototype._siteRedirect = function () {
-            //window.location.href = 'start.html?pocketguide_video='+ this._videoId +'_en&youtube=1';
-            window.location.href = 'start.html?youtube=1';
+            window.location.href = 'start.html?pocketguide_video=' + this._videoId + '_en&youtube=1';
         };
         TeaserVideoPlayer.prototype._createVideo = function () {
             var _this = this;
