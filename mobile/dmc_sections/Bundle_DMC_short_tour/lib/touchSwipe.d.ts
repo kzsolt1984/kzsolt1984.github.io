8d98f8200590f6d7eeb45118cf6984d33586d478
Zsolt Kecskés <kzsolt1984@gmail.com>
2015-11-27 11:19:09 +0100

    dmc text fix

---
+++ mobile/dmc_sections/Bundle_DMC_short_tour/lib/touchSwipe.d.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by Zsolt on 2015.10.10..
+ */
+
+///<reference path="jquery.d.ts"/>
+
+interface JQuery {
+    /**
+     * Creates a bxSlider from the current element.
+     * @param options
+     */
+    swipe(options?:any): any;
+}
